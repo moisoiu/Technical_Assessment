583223048debf6806fde07498c41ba2a85aa69e2
Moisoiu Mircea <moisoiumirceamihai@gmail.com>
2024-07-25 21:09:06 +0300

    Added benchmark tests and added tests. RoverOld is better in terms of memory allocation and cpu usage

---
+++ RoverApp/Benchmark/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Benchmark;
+using BenchmarkDotNet.Running;
+
+var summary = BenchmarkRunner.Run<RoverBenchmark>();
+
+Console.ReadLine();
---
+++ RoverApp/Benchmark/RoverBenchmark.cs
@@ -0,0 +1,58 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using RoverApp;
+
+namespace Benchmark;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class RoverBenchmark
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private RoverOld _roverOld;
+    private Rover _rover;
+    private RoverV2 _roverV2;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _roverOld = new RoverOld(0, 0, 'F');
+        _rover = new Rover(0, 0, DirectionEnum.North);
+
+        var strategies = new Dictionary<MovementEnum, IMovementStrategy>
+            {
+                { MovementEnum.Left, new TurnLeftStrategy() },
+                { MovementEnum.Right, new TurnRightStrategy() },
+                { MovementEnum.Forward, new MoveForwardStrategy() },
+                { MovementEnum.Backward, new MoveBackwardStrategy() }
+            };
+
+        var movementHandler = new MovementHandler(strategies);
+
+        _roverV2 = new RoverV2(0, 0, DirectionEnum.North, movementHandler);
+    }
+
+    [Benchmark]
+    public string MoveAndGetPossition_RoverOld()
+    {
+        _roverOld.Move("FRFRFRF");
+        return _roverOld.GetPosition();
+    }
+
+
+    [Benchmark]
+    public string MoveAndGetPossition_Rover()
+    {
+        _rover.Move(MovementEnum.Forward, MovementEnum.Right, MovementEnum.Forward, MovementEnum.Right, MovementEnum.Forward, MovementEnum.Right, MovementEnum.Forward);
+        return _rover.GetPosition();
+    }
+
+    [Benchmark]
+    public string MoveAndGetPossition_RoverV2()
+    {
+        _roverV2.Move(MovementEnum.Forward, MovementEnum.Right, MovementEnum.Forward, MovementEnum.Right, MovementEnum.Forward, MovementEnum.Right, MovementEnum.Forward);
+        return _rover.GetPosition();
+    }
+}
